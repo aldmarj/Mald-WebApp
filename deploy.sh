15119ad51d67e7d79f4d66a5c0f60d4f0062445d
DomHoney <ddhoney97@hotmail.co.uk>
2017-10-19 17:07:26 +0100

    add port option to ssh-keyscan

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ eval $(ssh-agent -s)
 ssh-add <(echo "$DEPLOY_SSH_PRIVATE_KEY")
 # Add deploy server host key to known_hosts to avoid ssh asking to trust new host
 mkdir -p ~/.ssh
-ssh-keyscan -H $DEPLOY_SERVER_IP >> ~/.ssh/known_hosts
+ssh-keyscan -H -p 8421 $DEPLOY_SERVER_IP >> ~/.ssh/known_hosts
 
 scp -r ./docker-compose.autodeploy.yml $DEPLOY_SERVER_USER@$DEPLOY_SERVER_IP:~/
 ssh $DEPLOY_SERVER_USER@$DEPLOY_SERVER_IP "docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN ${CI_REGISTRY};"\
