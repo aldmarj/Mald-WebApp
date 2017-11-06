374d9c32d36ff9dbc30e6655feac4963379850ec
DomHoney <ddhoney97@hotmail.co.uk>
2017-11-06 23:38:08 +0000

    #6-initial-log-work-impl: Configure RestTemplate rootUri for api

--- src/main/java/website/SpringBootWebApplication.java
+++ src/main/java/website/SpringBootWebApplication.java
@@ -1,12 +1,25 @@
 package website;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+  @Value("api.rootUri")
+  private String apiRootUri;
+
   public static void main(String[] args) {
     SpringApplication.run(SpringBootWebApplication.class, args);
   }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.rootUri(apiRootUri).build();
+  }
+
 }
