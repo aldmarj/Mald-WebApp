bb2606f464babc02db2bf93ed6eb68c90a06a9ee
DomHoney <ddhoney97@hotmail.co.uk>
2017-12-06 16:21:30 +0000

    #6-initial-log-work-impl: Removed seconds from global date format

--- src/main/java/website/config/WebMvcConfig.java
+++ src/main/java/website/config/WebMvcConfig.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-  public static final String MOMENTJS_DATE_FORMAT = "DD-MM-YYYY HH:mm:ss";
+  public static final String MOMENTJS_DATE_FORMAT = "DD-MM-YYYY HH:mm";
+  public static final String SIMPLE_DATE_FORMAT = "dd-MM-yy HH:mm";
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
-    registry.addFormatter(new DateFormatter("dd-MM-yy HH:mm:ss"));
+    registry.addFormatter(new DateFormatter(SIMPLE_DATE_FORMAT));
   }
 
 }
