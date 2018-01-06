1f9515ed8c5144978cebf2a53c84a0c5c529bc7f
Lawrence <lawrencejcoles@gmail.com>
2018-01-06 14:07:01 +0000

    #31 merged in latest code.

--- src/main/java/website/service/ClientService.java
+++ src/main/java/website/service/ClientService.java
@@ -21,45 +21,58 @@ public class ClientService extends BaseService {
 	 * 
 	 * @return a list of all the clients for a business.
 	 */
-  public Collection<Client> getClients(String businessTag) {
-    Client[] response = restTemplate
-        .getForObject("/business/{businessTag}/client/",
-            Client[].class,
-            businessTag
-        );
+	public Collection<Client> getClients(String businessTag) {
+		Client[] response = 
+				restTemplate.getForObject("/business/{businessTag}/client/", 
+						Client[].class, 
+						businessTag);
 
-    return Arrays.asList(response);
-  }
+		return Arrays.asList(response);
+	}
 
- /**
-   * Get the employees who have worked the most for a time frame.
-   * 
-   * @param businessTag - The business context,
-   * @return the employees of have worked the most for a time frame. Most first.
-   */
-  public Collection<Client> getTopNClientsForCurrentMonth(String businessTag, int n) {    	
-  	long startMonth = DateUtils.getStartOfCurrentMonthInMillis();
-  	long endMonth = DateUtils.getEndOfCurrentMonthInMillis();
-  	
-  	Client[] response = restTemplate
-              .getForObject("/business/{businessTag}/client/mostWorked/top/1/{n}/between/{startMonth}/{endMonth}",
-            		  Client[].class,
-                      businessTag,
-                      n,
-                      startMonth,
-                      endMonth
-              );
+	/**
+	 * Get the employees who have worked the most for a time frame.
+	 * 
+	 * @param businessTag
+	 *            - The business context,
+	 * @return the employees of have worked the most for a time frame. Most
+	 *         first.
+	 */
+	public Collection<Client> getTopNClientsForCurrentMonth(String businessTag, int n) {
+		long startMonth = DateUtils.getStartOfCurrentMonthInMillis();
+		long endMonth = DateUtils.getEndOfCurrentMonthInMillis();
 
-      return Arrays.asList(response);
-  }
+		Client[] response = 
+				restTemplate.getForObject(
+				"/business/{businessTag}/client/mostWorked/top/1/{n}/between/{startMonth}/{endMonth}", 
+				Client[].class,
+				businessTag, 
+				n, 
+				startMonth, 
+				endMonth);
 
-  
-    public ResponseEntity<String> addClient(String businessTag, Client client) {
-	    HttpHeaders headers = new HttpHeaders();
-	    HttpEntity<Client> request = new HttpEntity<Client>(client, headers);
-	    ResponseEntity<String> response =
-	        restTemplate.exchange("/business/{businessTag}/client", HttpMethod.POST,
-	            request, String.class, businessTag);
-	    return response;
-  }
+		return Arrays.asList(response);
+	}
+
+	/**
+	 * Add a client to the API.
+	 * 
+	 * @param businessTag - The business context.
+	 * @param client - The client to add.
+	 * @return the response of the query.
+	 */
+	public ResponseEntity<String> addClient(String businessTag, Client client) {
+		
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<Client> request = new HttpEntity<Client>(client, headers);
+		
+		ResponseEntity<String> response = 
+				restTemplate.exchange("/business/{businessTag}/client", 
+					HttpMethod.POST,
+					request, 
+					String.class,
+					businessTag);
+		
+		return response;
+	}
 }
